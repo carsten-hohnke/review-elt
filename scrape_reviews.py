86d96cfc1f0be4716cd30b7a6b94534658cf9fd2
Carsten <carsten.hohnke@gmail.com>
2023-05-03 13:22:03 -0400

    Major commit adding packages needed to debug gpt code

--- scrape_reviews.py
+++ scrape_reviews.py
@@ -2,19 +2,20 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
 import csv
 
 # Set up headless browser
 options = Options()
-options.headless = True
+options.headless = False
 
 # Set up driver
 driver = webdriver.Chrome(options=options)
 
 # Set up URL
-url = 'https://www.trustpilot.com/review/branchapp.com'
+url = 'https://www.trustpilot.com/review/ourbranch.com'
 
 # Set up CSV file
 filename = 'branch_reviews.csv'
@@ -27,6 +28,8 @@ with open(filename, 'w', newline='', encoding='utf-8') as f:
 while True:
     # Get page
     driver.get(url)
+    #get the button to click
+
     time.sleep(5)
     
     # Extract HTML content
@@ -34,16 +37,21 @@ while True:
     soup = BeautifulSoup(html, 'html.parser')
     
     # Extract reviews
-    reviews = soup.find_all('div', {'class': 'review__content'})
+    reviews = soup.find_all('article', {'class': 'paper_paper__1PY90 paper_outline__lwsUX card_card__lQWDv styles_reviewCard__hcAvl'})
     for review in reviews:
         # Extract review text
-        review_text = review.find('p', {'class': 'review-content__text'}).text.strip()
+        review_text = review.find('p', {'class': 'typography_body-l__KUYFJ typography_appearance-default__AAY17 typography_color-black__5LYEn'}).text.strip()
+        review_text = review_text.replace(',', '')
         
-        # Extract rating
-        rating = review.find('img', {'class': 'star-rating__star'}).get('alt').strip()
+        # Extract rating. Note: rating is in the form of stars, so we need to extract the alt text from the image tag following the div tag of class 'star-rating_starRating__4rrcf star-rating_medium__iN6Ty'
+        rating = review.find('div', {'class': 'star-rating_starRating__4rrcf star-rating_medium__iN6Ty'}).find('img').get('alt').strip()
+        rating_strip = len("Rated ")
+        rating = rating[rating_strip:rating_strip+1]
         
         # Extract date
-        date = review.find('div', {'class': 'review-content-header__dates'}).find_all('div')[1].text.strip()
+        date = review.find('p', {'class': 'typography_body-m__xgxZ_ typography_appearance-default__AAY17 typography_color-black__5LYEn'}).text.strip()
+        date_strip = len("Date of experience: ")
+        date = date[date_strip:]
         
         # Write to CSV file
         row = [review_text, rating, date]
@@ -52,9 +60,9 @@ while True:
             writer.writerow(row)
     
     # Check if there are more reviews
-    load_more_button = driver.find_element_by_xpath('//button[@data-qa="reviews-show-more-button"]')
+    load_more_button = driver.find_element(By.NAME, 'pagination-button-next')
     if load_more_button:
-        load_more_button.click()
+        driver.execute_script("arguments[0].click();", load_more_button)
         time.sleep(5)
     else:
         break
